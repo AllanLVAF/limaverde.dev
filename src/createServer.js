0724efd3fdf7ea0bff46c5e42686335eaa05e3f4
Allan LimaVerde <allan@provi.com.br>
2021-12-28 13:47:36 -0300

    Fixing SSL key and chain order

--- src/createServer.js
+++ src/createServer.js
@@ -8,8 +8,8 @@ const createServer = (app, isProduction) => {
   const fs = require('fs')
 
   const options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/limaverde.dev/fullchain.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/limaverde.dev/privkey.pem')
+    key: fs.readFileSync('/etc/letsencrypt/live/limaverde.dev/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/limaverde.dev/fullchain.pem')
   }
 
   return https.createServer(options, app)
