d80bcc893298da1072e9cfbbd405a975d5522b73
Allan LimaVerde <allanlvaf@gmail.com>
2022-07-30 23:24:47 -0300

    Minor cleanups

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const cors = require('cors')
-const throng = require('throng')
 const express = require('express')
 const createServer = require('./createServer')
 
@@ -9,19 +8,17 @@ const portProd = 443
 const { NODE_ENV } = process.env
 const isProduction = NODE_ENV === 'production'
 
-const start = () => {
-  const app = express()
-  const server = createServer(app, isProduction)
+const app = express()
+const server = createServer(app, isProduction)
 
-  app.use(cors())
-  app.use(express.json({}))
-  app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(express.json({}))
+app.use(express.urlencoded({ extended: true }))
 
-  app.use('/', express.static('./src/build'))
+app.use('/', express.static('./src/build'))
 
-  const port = isProduction ? portProd : portLocal
+const port = isProduction ? portProd : portLocal
 
-  server.listen(port, () => {
-    console.log(`We are live on ${port}`)
-  })
-}
+server.listen(port, () => {
+  console.log(`We are live on ${port}`)
+})
